30f54bc43cc7e5eb445764b924000d85f2dc9abd
root <root@puppet.c.augmented-form-103908.internal>
2015-09-24 02:00:01 +0000

    Thu Sep 24 02:00:01 UTC 2015 commit

--- FAIL-over.sh
+++ FAIL-over.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+US=''
+ASIA=''
 SWITCH=$(sed -ne 's/.*mysql-\(.*\).kitchenseeker.com./\1/p' /etc/bind/db.kitchenseeker.com)
 
 if [ $SWITCH == 'active' ]; then
 	SWITCH='passive'
+	#./ddns $US
 elif [ $SWITCH == 'passive' ]; then
 	SWITCH='active'
+	#./ddns $ASIA
 else
-	echo 'What that fuck is going on here'
+	echo 'What that heck is going on here'
 fi
 
 sed -i "s/mysql-.*\.kitchenseeker\.com/mysql-$SWITCH\.kitchenseeker\.com/g" /etc/bind/db.kitchenseeker.com
