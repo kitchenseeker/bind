f7cfc31129308cbd28b3369aa3df2a4cd33af2cb
root <root@puppet.c.augmented-form-103908.internal>
2015-10-06 10:35:54 +0000

    Tue Oct  6 10:35:54 UTC 2015 commit

---
+++ ddns.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import keyring
+from keyring.backend import *
+import sys
+
+hostname='kitchenseeker.ddns.net'
+username='kitchenseeker'
+new_ip=str(sys.argv[1])
+
+keyring.set_keyring(UncryptedFileKeyring())
+password=keyring.get_password('ddns-no-ip','root')
+
+command="curl \"https://%s:%s@dynupdate.no-ip.com/nic/update?hostname=%s&myip=%s\"" % (username,password,hostname,new_ip)
+os.system(command)
